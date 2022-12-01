7a02541abc51eec334974fe571d00b4284ad21fb
Pedro Sousa <pedronsousa@gmail.com>
2022-12-01 18:33:08 +0000

    Day 1 Part TWO

--- src/main/kotlin/Day1.kt
+++ src/main/kotlin/Day1.kt
@@ -1,17 +1,22 @@
 class Day1(testing: Boolean = false) : Day(1, testing) {
 
-    override fun part1(): SolutionReturnType {
-        val sums = getInputFile(DayPart.ONE).useLines {lines->
-            lines.fold(listOf(0)) {sums, value ->
+    override fun part1(): SolutionReturnType =
+        getElvesCalories().max().toString()
+
+
+    override fun part2(): SolutionReturnType {
+        return getElvesCalories().sortedDescending().take(3).sum().toString()
+    }
+
+    private fun getElvesCalories(): List<Int> =
+        getInputFile(DayPart.ONE).useLines { lines ->
+            lines.fold(listOf(0)) { sums, value ->
                 when {
                     value.isEmpty() -> sums + 0
                     else -> sums.dropLast(1) + (sums.last() + value.toInt())
                 }
             }
         }
-
-        return sums.max().toString()
-    }
 }
 
 
