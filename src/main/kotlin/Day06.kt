698f44978210f184f76362824ff03c87bd63d3eb
Pedro Sousa <pedronsousa@gmail.com>
2022-12-06 22:46:33 +0000

    Simplify Day 06

--- src/main/kotlin/Day06.kt
+++ src/main/kotlin/Day06.kt
@@ -5,13 +5,11 @@ class Day06(testing: Boolean = false) : DaySolutions(6, testing) {
     override fun partTwo(): SolutionResult = findMarker(14)
 
     private fun findMarker(windowSize: Int): String {
-        var window = ""
         var marker = 0
-        for (c in input.first()) {
-            marker += 1
-            window += c
-            if (window.length > windowSize) window = window.substring(1)
-            if (window.toSet().size == windowSize) break
+        val signal = input.first()
+        for (i in windowSize until signal.length) {
+            marker = i
+            if (signal.substring(i - windowSize, i).toSet().size == windowSize) break
         }
 
         return marker.toString()
