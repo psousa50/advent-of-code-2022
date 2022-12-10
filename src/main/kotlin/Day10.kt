08d911b7503c66d5de52170320f286db11c66662
Pedro Sousa <pedronsousa@gmail.com>
2022-12-10 12:02:19 +0000

    Refactor

--- src/main/kotlin/Day10.kt
+++ src/main/kotlin/Day10.kt
@@ -1,24 +1,22 @@
 class Day10(testing: Boolean = false) : DaySolutions(10, testing) {
     override fun partOne(): SolutionResult =
-        input.map { parse(it) }
-            .fold(Cpu()) { cpu, instruction ->
-                cpu.execute(instruction)
-            }
-            .signalStrengths.sum()
+        input
+            .map { parse(it) }
+            .fold(VideoSystem()) { system, instruction -> system.run(instruction) }
+            .cpu.signalStrengths.sum()
             .bind()
 
     override fun partTwo(): SolutionResult =
-        input.map { parse(it) }
-            .fold(Cpu()) { cpu, instruction ->
-                cpu.execute(instruction)
-            }
+        input
+            .map { parse(it) }
+            .fold(VideoSystem()) { system, instruction -> system.run(instruction) }
             .crt.display()
             .bind()
 
     private fun parse(line: String): Instruction =
         when {
-            line.startsWith("noop") -> Instruction("noop")
-            else -> Instruction("addx", line.split(" ")[1].toInt())
+            line.startsWith("noop") -> Instruction(OP_NOOP)
+            else -> Instruction(OP_ADDX, line.split(" ")[1].toInt())
         }
 
     data class Instruction(
@@ -27,17 +25,27 @@ class Day10(testing: Boolean = false) : DaySolutions(10, testing) {
     ) {
         fun cycles(): Int =
             when (op) {
-                "noop" -> 1
+                OP_NOOP -> 1
                 else -> 2
             }
+    }
+
+    data class VideoSystem(
+        val cpu: Cpu = Cpu(),
+        val crt: Crt = Crt()
+    ) {
+        fun run(instruction: Instruction) =
+            VideoSystem(
+                cpu.execute(instruction),
+                (0 until instruction.cycles())
+                    .fold(crt) { crt, _ -> crt.drawPixel(cpu.xRegister) }
+            )
     }
 
     data class Cpu(
         val xRegister: Int = 1,
         val programCounter: Int = 0,
         val signalStrengths: List<Int> = listOf(),
-        val crt: Crt = Crt()
-
     ) {
         fun execute(instruction: Instruction): Cpu {
             val newProgramCounter = programCounter + instruction.cycles()
@@ -48,26 +56,24 @@ class Day10(testing: Boolean = false) : DaySolutions(10, testing) {
                     signalStrengths + signalEdge * xRegister
                 else
                     signalStrengths
-
-            val newCtr = (0 until instruction.cycles()).fold(crt) { crt, _ -> crt.drawPixel(xRegister) }
-
-            return Cpu(newValue, newProgramCounter, newSignalStrengths, newCtr)
+            return Cpu(newValue, newProgramCounter, newSignalStrengths)
         }
     }
 
     data class Crt(val pixels: String = "") {
-        fun display() = pixels.chunked(DISPLAY_WIDTH).joinToString (System.lineSeparator() )
-        fun drawPixel(xRegister: Int): Crt {
+        fun display() = pixels.chunked(DISPLAY_WIDTH).joinToString(System.lineSeparator())
+        fun drawPixel(spritePosition: Int): Crt {
             val beamPosition = pixels.length % DISPLAY_WIDTH
-            val pixel = if (beamPosition in xRegister - 1..xRegister + 1) "#" else "."
+            val pixel = if (beamPosition in spritePosition - 1..spritePosition + 1) "#" else "."
             return Crt(pixels + pixel)
         }
-
     }
 
     companion object {
         const val SIGNAL_START: Int = 20
         const val SIGNAL_WINDOW: Int = 40
         const val DISPLAY_WIDTH: Int = 40
+        const val OP_NOOP = "noop"
+        const val OP_ADDX = "addx"
     }
 }
