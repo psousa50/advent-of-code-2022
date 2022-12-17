aa17c8de31573b238d797d36faf0d0e70c1ff2f3
Pedro Sousa <pedronsousa@gmail.com>
2022-12-17 10:25:27 +0000

    Day 13 refactor

--- src/main/kotlin/Day13.kt
+++ src/main/kotlin/Day13.kt
@@ -1,56 +1,30 @@
+import java.lang.Integer.min
+
 class Day13(testing: Boolean = false) : DaySolutions(13, testing) {
     override fun partOne(): SolutionResult =
         input
-            .splitByBlankLine { lines -> lines.map { parse(it) } }
-            .map {
-                println("${it.first <= it.second} ${it.first}")
-                println("${it.first <= it.second} ${it.second}")
-                println("")
-//                println("RR ===================== ${it.first <= it.second}")
-                it
-            }
+            .splitByBlankLine { lines -> lines.map { parsePairOfPackets(it) } }
             .mapIndexed { i, pair -> if (pair.first <= pair.second) i + 1 else 0 }
             .sum()
             .bind()
 
-    private fun parse(lines: List<String>) =
+    private fun parsePairOfPackets(lines: List<String>) =
         Pair(
-            parsePacket(Parser(lines[0]).moveNext()),
-            parsePacket(Parser(lines[1]).moveNext()),
+            PacketParser().parse(lines[0]),
+            PacketParser().parse(lines[1]),
         )
 
-    private fun parsePacket(parser: Parser): Packet {
-        val packets = mutableListOf<Value>()
-        while (parser.peek != ']') {
-            val value =
-                if (parser.peek == '[')
-                    parsePacket(parser.moveNext())
-                else
-                    IntValue(parser.number())
-            packets.add(value)
-            parser.skip(',')
-        }
-        parser.skip(']')
-        parser.skip(',')
-        return Packet(packets)
-    }
-
-    sealed interface Value {
+    private sealed interface Value {
         val size: Int
-        fun toPacket(): Value
+        fun toPacket(): Packet
         operator fun get(i: Int): Value
-        operator fun compareTo(other: Value): Int
     }
 
     private data class IntValue(val value: Int) : Value {
         override val size get() = 1
         override fun get(i: Int) = this
         override fun toPacket() = Packet(listOf(this))
-        override fun compareTo(other: Value): Int =
-            when (other) {
-                is IntValue -> this.value.compareTo(other.value)
-                is Packet -> 0
-            }
+        operator fun compareTo(other: IntValue) = this.value.compareTo(other.value)
 
         override fun toString() = value.toString()
     }
@@ -63,53 +37,67 @@ class Day13(testing: Boolean = false) : DaySolutions(13, testing) {
         override fun get(i: Int) = packets[i]
         override fun toPacket() = this
 
-        override operator fun compareTo(other: Value): Int {
-            var bigger = false
-            var smaller = false
+        operator fun compareTo(other: Packet): Int {
+            val firstDifferent = packets.subList(0, min(size, other.size))
+                .mapIndexed { i, v -> Pair(v, other[i]) }
+                .firstOrNull { compare(it) != 0 }
 
-            @Suppress("LoopWithTooManyJumpStatements")
-            for (i in 0 until size) {
-                if (i >= other.size) {
-                    bigger = true
-                    break
-                }
-                val thisValue = if (this[i]::class != other[i]::class) this[i].toPacket() else this[i]
-                val otherValue = if (this[i]::class != other[i]::class) other[i].toPacket() else other[i]
-                if (thisValue > otherValue) {
-                    bigger = true
-                    break
-                }
-                if (thisValue < otherValue) {
-                    smaller = true
-                    break
-                }
-            }
-            return when {
-                bigger -> 1
-                smaller -> -1
-                else -> if (size < other.size) -1 else 0
+            return when (firstDifferent) {
+                null -> size.compareTo(other.size)
+                else -> compare(firstDifferent)
             }
         }
 
-        override fun toString() =
-            "[${packets.joinToString(",")}]"
+        private fun compare(pair: Pair<Value, Value>): Int {
+            val first = pair.first
+            val second = pair.second
+            return when {
+                first is IntValue && second is IntValue -> first.compareTo(second)
+                first is Packet && second is Packet -> first.compareTo(second)
+                else -> first.toPacket().compareTo(second.toPacket())
+            }
+        }
+
+        override fun toString() = "[${packets.joinToString(",")}]"
     }
 
-    private class Parser(val line: String) {
-        var cursor = 0
-
+    private class PacketString(
+        val line: String,
+        var cursor: Int = 0
+    ) {
         val end get() = cursor >= line.length
         val peek get() = line[cursor]
         fun moveNext() = also { cursor++ }
-        fun takeWhile(p: (c: Char) -> Boolean): String {
-            val s = line.substring(cursor).takeWhile(p)
-            cursor += s.length
-            return s
-        }
+        fun takeWhile(predicate: (c: Char) -> Boolean) =
+            line
+                .substring(cursor)
+                .takeWhile(predicate)
+                .also { cursor += it.length }
 
         fun skip(c: Char) = also { if (!end && peek == c) moveNext() }
 
         fun number() = takeWhile { it.isDigit() }.toInt()
     }
-}
 
+    private class PacketParser() {
+        fun parse(line: String) = parse(PacketString(line))
+        private fun parse(packetString: PacketString): Packet {
+            packetString.moveNext()
+            val packets = mutableListOf<Value>()
+            while (packetString.peek != ']') {
+                val value =
+                    if (packetString.peek == '[')
+                        parse(packetString)
+                    else
+                        IntValue(packetString.number())
+                packets.add(value)
+                packetString.skip(',')
+            }
+            packetString.skip(']')
+            packetString.skip(',')
+            return Packet(packets)
+        }
+
+
+    }
+}
